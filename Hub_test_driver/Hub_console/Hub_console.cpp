d260f57f46ceaf5b0de1f805250999cb69583cef
fgfl <frederick.gfl@gmail.com>
2014-10-28 15:41:28 -0700

    pushing missing files

---
+++ Hub_test_driver/Hub_console/Hub_console.cpp
@@ -0,0 +1,13 @@
+// Hub_console.cpp : Defines the entry point for the console application.
+//
+
+#include "Hub_sim.hpp"
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("testing\n");
+	return 0;
+}
+
