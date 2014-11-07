dd7a1056c26992a13bcc5cd4ccbfc9c8f057c77c
lordy1992 <jlord_2006@hotmail.com>
2014-11-06 23:28:06 -0800

    Dynamically generating list of labels and moving gestures to a ListBox, did some renaming

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/MyoSimulatorForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MyoSimGUI
+{
+    partial class MyoSimulatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendCommandButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveFilename = new System.Windows.Forms.TextBox();
+            this.commandChain = new System.Windows.Forms.TextBox();
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.gestureLabel = new System.Windows.Forms.Label();
+            this.addGestureButton = new System.Windows.Forms.Button();
+            this.gestureList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // sendCommandButton
+            // 
+            this.sendCommandButton.Location = new System.Drawing.Point(430, 272);
+            this.sendCommandButton.Name = "sendCommandButton";
+            this.sendCommandButton.Size = new System.Drawing.Size(75, 23);
+            this.sendCommandButton.TabIndex = 9;
+            this.sendCommandButton.Text = "Send";
+            this.sendCommandButton.UseVisualStyleBackColor = true;
+            this.sendCommandButton.Click += new System.EventHandler(this.sendCommandButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(430, 243);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // saveFilename
+            // 
+            this.saveFilename.Location = new System.Drawing.Point(12, 245);
+            this.saveFilename.Name = "saveFilename";
+            this.saveFilename.Size = new System.Drawing.Size(382, 20);
+            this.saveFilename.TabIndex = 11;
+            // 
+            // commandChain
+            // 
+            this.commandChain.Location = new System.Drawing.Point(12, 275);
+            this.commandChain.Name = "commandChain";
+            this.commandChain.ReadOnly = true;
+            this.commandChain.Size = new System.Drawing.Size(382, 20);
+            this.commandChain.TabIndex = 0;
+            this.commandChain.Tag = "";
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(430, 214);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.Size = new System.Drawing.Size(75, 23);
+            this.loadFileButton.TabIndex = 12;
+            this.loadFileButton.Text = "Load";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+            // 
+            // gestureLabel
+            // 
+            this.gestureLabel.AutoSize = true;
+            this.gestureLabel.Location = new System.Drawing.Point(12, 19);
+            this.gestureLabel.Name = "gestureLabel";
+            this.gestureLabel.Size = new System.Drawing.Size(77, 13);
+            this.gestureLabel.TabIndex = 15;
+            this.gestureLabel.Text = "Select Gesture";
+            // 
+            // addGestureButton
+            // 
+            this.addGestureButton.Location = new System.Drawing.Point(141, 83);
+            this.addGestureButton.Name = "addGestureButton";
+            this.addGestureButton.Size = new System.Drawing.Size(75, 23);
+            this.addGestureButton.TabIndex = 16;
+            this.addGestureButton.Text = "Add Gesture";
+            this.addGestureButton.UseVisualStyleBackColor = true;
+            this.addGestureButton.Click += new System.EventHandler(this.addGestureButton_Click);
+            // 
+            // gestureList
+            // 
+            this.gestureList.FormattingEnabled = true;
+            this.gestureList.Location = new System.Drawing.Point(15, 35);
+            this.gestureList.Name = "gestureList";
+            this.gestureList.Size = new System.Drawing.Size(120, 121);
+            this.gestureList.TabIndex = 17;
+            // 
+            // MyoSimulatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 307);
+            this.Controls.Add(this.gestureList);
+            this.Controls.Add(this.addGestureButton);
+            this.Controls.Add(this.gestureLabel);
+            this.Controls.Add(this.loadFileButton);
+            this.Controls.Add(this.saveFilename);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.sendCommandButton);
+            this.Controls.Add(this.commandChain);
+            this.Name = "MyoSimulatorForm";
+            this.Text = "Myo Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendCommandButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox saveFilename;
+        private System.Windows.Forms.TextBox commandChain;
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.Label gestureLabel;
+        private System.Windows.Forms.Button addGestureButton;
+        private System.Windows.Forms.ListBox gestureList;
+
+    }
+}
+
